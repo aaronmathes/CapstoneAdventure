ff7ddb25e967c88f4be881a6dd8f6d6e8bc91802
annarose2255 <daytonkaroll@outlook.com>
2022-01-04 12:01:42 -0500

    fixed validation on register

--- Capstone_Xavier/Models/RegisterModel.cs
+++ Capstone_Xavier/Models/RegisterModel.cs
@@ -11,7 +11,13 @@ namespace Capstone_Xavier.Models
         public int userValid = 0;
 
 
-        [Required]
+        [Required(ErrorMessage = "Password is required.")]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[#@$%!?^&*\-+]).*$", ErrorMessage = "Invalid Password. Passwords must contain: " +
+            "<br> &ensp; \u2022 One or more captial letters " +
+            "<br> &ensp; \u2022 One or more lower case letters" +
+            "<br> &ensp; \u2022 One or more numbers (0-9)" +
+            "<br> &ensp; \u2022 One or more special characters (#, ?, !, @, $, %, ^, &, *, -, +)")]
+        [StringLength(20, MinimumLength = 8, ErrorMessage = "Password must be at least 8 and no more than 20 characters long.")]
         public string password { get; set; }
         public int passValid = 0;
 
